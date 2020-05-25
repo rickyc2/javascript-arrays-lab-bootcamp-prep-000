37e69a71eec4d1c1025f7c1ac7dc5a46d80aeb3a
darling-thread-6760 <mistawilliams210@gmail.com>
2020-05-25 02:17:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
-function destructivelyAppendKitten(kittens){
-kittens.unshift();
+function destructivelyAppendKitten(name){
+kittens.unshift(name);
 return kittens
 }
